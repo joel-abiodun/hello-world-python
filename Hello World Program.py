1132e59748f89a42b008828da2aef67574a27af8
joel-abiodun <joelabiodun321@gmail.com>
2026-01-16 01:17:58 +0100

    Add files via upload

---
+++ Hello World Program.py
@@ -0,0 +1,11 @@
+# Hello World Program
+# Name: Joel Abiodun
+# Matric Number: 25/18070
+# Course: Computer Science
+# Institution: Caleb University
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
